28ff6f86f2d9c85ede8ea5743d518a74d0d57f0d
hoybin <hoybin@foxmail.com>
2018-11-10 17:18:57 +0800

    first day

---
+++ demo/2.var.php
@@ -0,0 +1,6 @@
+<?php
+$name = 'alisy';
+echo $name;
+unset($name); // 销毁
+echo $name;
+?>
---
+++ demo/3.concat.php
@@ -0,0 +1,11 @@
+<?php
+$name = 'andy';
+$age = 36;
+
+echo "andy age is $age";
+
+echo '<hr>';
+
+echo 'andy age is ' . $age;
+
+?>
